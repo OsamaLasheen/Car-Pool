9f3f7b2ac5d60fc18ac8aa7de80077861e6cdd2f
osama <osamalasheeno@gmail.com>
2023-12-24 03:24:38 +0200

    Done

---
+++ driver_car_pool/.dart_tool/flutter_build/dart_plugin_registrant.dart
@@ -0,0 +1,72 @@
+//
+// Generated file. Do not edit.
+// This file is generated from template in file `flutter_tools/lib/src/flutter_plugins.dart`.
+//
+
+// @dart = 3.1
+
+import 'dart:io'; // flutter_ignore: dart_io_import.
+import 'package:sqflite/sqflite.dart';
+import 'package:sqflite/sqflite.dart';
+import 'package:package_info_plus/package_info_plus.dart';
+import 'package:sqflite/sqflite.dart';
+import 'package:package_info_plus/package_info_plus.dart';
+
+@pragma('vm:entry-point')
+class _PluginRegistrant {
+
+  @pragma('vm:entry-point')
+  static void register() {
+    if (Platform.isAndroid) {
+      try {
+        SqflitePlugin.registerWith();
+      } catch (err) {
+        print(
+          '`sqflite` threw an error: $err. '
+          'The app may not function as expected until you remove this plugin from pubspec.yaml'
+        );
+      }
+
+    } else if (Platform.isIOS) {
+      try {
+        SqflitePlugin.registerWith();
+      } catch (err) {
+        print(
+          '`sqflite` threw an error: $err. '
+          'The app may not function as expected until you remove this plugin from pubspec.yaml'
+        );
+      }
+
+    } else if (Platform.isLinux) {
+      try {
+        PackageInfoPlusLinuxPlugin.registerWith();
+      } catch (err) {
+        print(
+          '`package_info_plus` threw an error: $err. '
+          'The app may not function as expected until you remove this plugin from pubspec.yaml'
+        );
+      }
+
+    } else if (Platform.isMacOS) {
+      try {
+        SqflitePlugin.registerWith();
+      } catch (err) {
+        print(
+          '`sqflite` threw an error: $err. '
+          'The app may not function as expected until you remove this plugin from pubspec.yaml'
+        );
+      }
+
+    } else if (Platform.isWindows) {
+      try {
+        PackageInfoPlusWindowsPlugin.registerWith();
+      } catch (err) {
+        print(
+          '`package_info_plus` threw an error: $err. '
+          'The app may not function as expected until you remove this plugin from pubspec.yaml'
+        );
+      }
+
+    }
+  }
+}
